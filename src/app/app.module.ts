6b796f96ee7751f314056764c13e6587aeddb291
debart-nathan <denathan@laposte.net>
2022-12-22 12:18:14 +0100

    app.module: removing not yet created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { EventComponent } from './event/event.component';
-import { TournComponent } from './tourn/tourn.component';
-import { TournCreationComponent } from './tourn-creation/tourn-creation.component';
-import { EventCreationComponent } from './event-creation/event-creation.component';
-import { MatchComponent } from './match/match.component';
-import { MatchEditionComponent } from './match-edition/match-edition.component';
 import { StageCreationComponent } from './stage-creation/stage-creation.component';
-import { InscriptionComponent } from './inscription/inscription.component';
 import { RouterModule } from '@angular/router';
 
 
@@ -22,14 +15,8 @@ import { RouterModule } from '@angular/router';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    EventComponent,
-    TournComponent,
-    TournCreationComponent,
-    EventCreationComponent,
-    MatchComponent,
-    MatchEditionComponent,
     StageCreationComponent,
-    InscriptionComponent,
+
   ],
   imports: [
     BrowserModule,
